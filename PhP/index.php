3ac5f34bdc33f4a555f2bc4a675400aafe4703ec
Leo Faleiro <leonardofaleiro81@gmail.com>
2025-02-17 18:44:23 +0100

    V.0.0.1 --Started with Log in screen

--- PhP/index.php
+++ PhP/index.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chirpify</title>
+    <link rel="stylesheet" href="../CSS/main.css">
 </head>
 <body>
 
+
+<div class="Parent">
+    <h1 id = "Title">Chirpify</h1>
+    <div class="LogIn">
+        <form action="" class="Form">
+
+            <h2 class="CenterItem">Log in</h2>
+            <label for="Email"></label>
+            <input type="email" id="Email" name="Email" placeholder="Email or Username" required>
+
+            <label for="Password"></label>
+            <input type="password" id="Password" name="Password" placeholder="Password" required>
+
+        </form>
+    </div>
+</div>
 
 </body>
 </html>
