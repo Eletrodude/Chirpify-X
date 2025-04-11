f169f4708c1dc3afaab2400c011f24ffe4c13ac7
Alhaji0 <hseen.al.haji05@gmail.com>
2025-04-11 19:29:55 +0200

    bug

--- PhP/Main.php
+++ PhP/Main.php
@@ -10,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php 
     session_start();
+    $cuon = $_SESSION['Username'];
     if ($_SESSION['Username'] == null) {
         $_SESSION['Error'] = 'You are not logged in!';
         header("Location: ../index.php");
@@ -22,80 +23,10 @@
 
     
     <div class="row" >              
-                    <div class=" SideBar  col-2" >
-                            <div  class="Logo">
-                                       
-                                    <h1 class="nav-item"> Chirpify</h1>
-                            </div>
-                            <div class="Links">
-                                <ul class="navbar-nav ">
-                                    <li class="nav-item">
-                                    <a class="nav-link"  href="#">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">About</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">Explore</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">Notifications</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">Messages</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">Settings</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">Help</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">Legal</a>
-                                    </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" href="#">loguit</a>
-                                    </li>
-                                </ul>
-                            </div>
-
-    <?php require '../Partials/Sidebar.php'?>
-        <div class="Posts  col-7">
-
-            <div class="all-divs">
-                <div class="Buttons ">
-                    <div class="Button1 ">
-                        <button id="Button1" class="btn btn-outline-secondary">For You</button>
-                    </div>
-                    <div class="Button2">
-                        <button id="Button2" class="btn btn-outline-secondary">Following</button>
-                    </div>
-                </div>
 
 
-            </div>
-
-        </div>
-
-        <div class="SideBar2  col-3">
-            <div class="SearchBar">
-                <form action="">
-
-                    <input class="form-control me-2" type="search" name="Search" id="Search">
-                    <button class="btn btn-outline-primary" type="submit">Search</button>
-                </form>
-
-                <div class="acounts">
-                    <h2>Voorgesteld voor jou</h2>
-                    <div class="user-container">
-                        <div class="user">
-                            <img src="../Assets/Images/pexels-pixabay-268533.jpg" alt="Account1" class="img-fluid rounded-circle">
-                            <h3>Account</h3>
-                        </div>
-                                    
-                            
-                        
-                            
+                        <?php require '../Partials/Sidebar.php'?>
+                                                        
                         <div class="Posts  col-7">
 
                              <div class="all-divs">
@@ -171,7 +102,7 @@
                             <div class="SearchBar">
                                 <div class="user">
                                         <a href="#">
-                                        <?php echo $cuon; ?> 
+                                        <?php echo $cuon ?> 
     
 
                                         
